3cbe621aed7846c2c4343ed97ac835f046daa567
mayuran-deriv <balamayuran@deriv.com>
2025-02-12 12:08:50 +0400

    discover router added

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,26 +1,35 @@
 import Feed from "@pages/feed";
 import Login from "@pages/login";
+import Discover from "@pages/discover";
 import ProtectedRoute from "@/components/ProtectedRoute";
 
 export const routes = [
-    {
-        path: "/login",
-        element: <Login />,
-    },
-    {
-        path: "/",
-        element: (
-            <ProtectedRoute>
-                <Feed />
-            </ProtectedRoute>
-        ),
-    },
-    {
-        path: "/feed",
-        element: (
-            <ProtectedRoute>
-                <Feed />
-            </ProtectedRoute>
-        ),
-    },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/",
+    element: (
+      <ProtectedRoute>
+        <Feed />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/feed",
+    element: (
+      <ProtectedRoute>
+        <Feed />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/discover",
+    element: (
+      <ProtectedRoute>
+        <Discover />
+      </ProtectedRoute>
+    ),
+  },
 ];
