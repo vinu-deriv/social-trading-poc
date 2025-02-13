8b75c09d574a3e38391427109900e9d2e8bb3fb7
vinu-deriv <vinu@regentmarkets.com>
2025-02-13 16:58:54 +0400

    fix: fix in docker-frontend file

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-            "@types": path.resolve(__dirname, "./src/types"),
-            "@components": path.resolve(__dirname, "./src/components"),
-            "@modules": path.resolve(__dirname, "./src/modules"),
-            "@pages": path.resolve(__dirname, "./src/pages"),
-            "@utils": path.resolve(__dirname, "./src/utils"),
-            "@constants": path.resolve(__dirname, "./src/constants"),
-            "@layouts": path.resolve(__dirname, "./src/layouts"),
-        },
-    },
+        alias: [
+            { find: '@', replacement: '/src' },
+            { find: '@types', replacement: '/src/types' },
+            { find: '@components', replacement: '/src/components' },
+            { find: '@modules', replacement: '/src/modules' },
+            { find: '@pages', replacement: '/src/pages' },
+            { find: '@utils', replacement: '/src/utils' },
+            { find: '@constants', replacement: '/src/constants' },
+            { find: '@layouts', replacement: '/src/layouts' }
+        ]
+    }
 });
