909f691f619c161da98a754ea131db6a84dfc9b7
mayuran-deriv <balamayuran@deriv.com>
2025-02-13 19:03:43 +0400

    trend svg added

--- src/pages/discover/components/TrendingAssets/TrendingAssets.tsx
+++ src/pages/discover/components/TrendingAssets/TrendingAssets.tsx
@@ -1,4 +1,6 @@
 import { useMemo } from "react";
+import ArrowUpTrend from "../../../../assets/icons/ArrowUpTrend";
+import ArrowDownTrend from "../../../../assets/icons/ArrowDownTrend";
 import "./TrendingAssets.css";
 
 interface Asset {
@@ -65,31 +67,9 @@ export default function TrendingAssets({
                 }`}
               >
                 {asset.direction === "up" ? (
-                  <svg
-                    width="16"
-                    height="16"
-                    viewBox="0 0 16 16"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M8 3.33334L8.94281 2.39053L8 1.44772L7.05719 2.39053L8 3.33334ZM7.33333 13.3333C7.33333 13.7015 7.63181 14 8 14C8.36819 14 8.66667 13.7015 8.66667 13.3333L7.33333 13.3333ZM12.9428 6.39053L8.94281 2.39053L7.05719 4.27615L11.0572 8.27615L12.9428 6.39053ZM7.05719 2.39053L3.05719 6.39053L4.94281 8.27615L8.94281 4.27615L7.05719 2.39053ZM7.33333 3.33334L7.33333 13.3333L8.66667 13.3333L8.66667 3.33334L7.33333 3.33334Z"
-                      fill="currentColor"
-                    />
-                  </svg>
+                  <ArrowUpTrend />
                 ) : (
-                  <svg
-                    width="16"
-                    height="16"
-                    viewBox="0 0 16 16"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M8 12.6667L7.05719 13.6095L8 14.5523L8.94281 13.6095L8 12.6667ZM8.66667 2.66667C8.66667 2.29848 8.36819 2 8 2C7.63181 2 7.33333 2.29848 7.33333 2.66667L8.66667 2.66667ZM3.05719 9.60947L7.05719 13.6095L8.94281 11.7239L4.94281 7.72386L3.05719 9.60947ZM8.94281 13.6095L12.9428 9.60947L11.0572 7.72386L7.05719 11.7239L8.94281 13.6095ZM8.66667 12.6667L8.66667 2.66667L7.33333 2.66667L7.33333 12.6667L8.66667 12.6667Z"
-                      fill="currentColor"
-                    />
-                  </svg>
+                  <ArrowDownTrend />
                 )}
                 {Math.abs(asset.changePercentage)}%
               </span>
