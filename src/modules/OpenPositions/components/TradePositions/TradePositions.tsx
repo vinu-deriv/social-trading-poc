94300d7126760ad0be9ab59262fb37971464f14b
vinu-deriv <vinu@regentmarkets.com>
2025-02-14 12:17:49 +0400

    fix: fix for docker build

--- src/modules/OpenPositions/components/TradePositions/TradePositions.tsx
+++ src/modules/OpenPositions/components/TradePositions/TradePositions.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Table from "@/components/Table";
 import { Contract } from "@/types/contract.types";
 import { positionsTableColumns, TradeType } from "../../constants";
--- src/modules/OpenPositions/components/index.ts
+++
@@ -1,5 +0,0 @@
-import { OptionsTable } from "./OptionsTable";
-import { MultipliersTable } from "./MultipliersTable";
-import { AccumulatorsTable } from "./AccumulatorsTable";
-
-export { OptionsTable, MultipliersTable, AccumulatorsTable };
